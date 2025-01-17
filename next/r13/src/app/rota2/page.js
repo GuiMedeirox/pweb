25ecf770a1c030a030bfb29ff1261ebcaa0407ae
Guilherme <guimedeiroxdev@gmail.com>
2025-01-16 23:41:46 -0300

    Nattanzinho Lima: No Doze

---
+++ next/r13/src/app/rota2/page.js
@@ -0,0 +1,11 @@
+import { Saudacao, Despedida } from '../page';
+
+export default function Rota2() {
+  return (
+    <div>
+      <h1>Bem-vindo à Rota 2!</h1>
+      <Saudacao />
+      <Despedida />
+    </div>
+  );
+}
